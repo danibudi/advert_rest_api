3bf20e8baca487c1ff5688fee443ceb16f5789ea
Dani <dani.budinova@spreadpointe.com>
2014-08-24 13:31:15 +0300

    git init migrations models

---
+++ zariba_test/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^ditributor/', include('ditributor.urls', namespace="ditributor")),
+    url(r'^advertisement/', include('advertisement.urls', namespace="advertisement")),
+    url(r'^admin/', include(admin.site.urls)),
+)
